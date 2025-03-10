3acba592c7f7d03a79c13903c0ed721b8b690a4a
bro875 <redfriend1989@gmail.com>
2025-03-10 17:30:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -10,41 +10,5 @@ document.getElementById("map").addEventListener("click", function(event) {
     // Hide map and start countdown
     document.getElementById("map-container").classList.add("hidden");
     let countdownElement = document.getElementById("countdown");
-    countdownElement.classList.remove("hidden");
+    countdownElement.classList.remove("
 
-    let timeLeft = 5;
-    countdownElement.innerText = "Launching in " + timeLeft + "...";
-
-    let countdownInterval = setInterval(function() {
-        timeLeft--;
-        countdownElement.innerText = "Launching in " + timeLeft + "...";
-        
-        if (timeLeft === 0) {
-            clearInterval(countdownInterval);
-            countdownElement.classList.add("hidden");
-
-            // Play explosion sound
-            document.getElementById("nuke-sound").play();
-
-            // Show explosion at clicked location
-            let explosion = document.getElementById("explosion");
-            explosion.style.left = x + "px";
-            explosion.style.top = y + "px";
-            explosion.style.display = "block";
-
-            // Show "Launch Successful!" after explosion
-            setTimeout(function() {
-                explosion.style.display = "none";
-                let successMessage = document.getElementById("success-message");
-                successMessage.classList.remove("hidden");
-
-                // Hide success message after 5 seconds
-                setTimeout(function() {
-                    successMessage.classList.add("hidden");
-                    document.getElementById("launch-button").style.display = "block"; // Reset button
-                }, 5000);
-
-            }, 1000); // Explosion lasts for 1 second
-        }
-    }, 1000);
-});
